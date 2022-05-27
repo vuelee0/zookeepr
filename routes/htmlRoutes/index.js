1c0d0ad5999b272a0d2900855bf0d18af1791793
Vue Lee <vuelee09@gmail.com>
2022-05-26 22:11:05 -0500

    modularize file

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const router = require('express').Router();
+
+
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+router.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/animals.html'));
+});
+
+router.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+});
+// wildcard route, will redirect any route not previously defined above
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+
+
+module.exports = router;
